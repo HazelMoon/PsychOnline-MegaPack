2ff54d62598bb8314488a1fa218e709636c20763
HazelMoon <comrad112@gmail.com>
2023-11-20 23:34:50 -0500

    Baka

--- PsychOnline-ModPack/custom_events/gagaboobs.lua
+++
@@ -1,7 +0,0 @@
-function onEvent(eventName, value1, value2)
-	if eventName == 'gagaboobs' then
-		setProperty('ratsAss.visible',true)
-		setProperty('jackettt.visible',true)
-		doTweenX('rectum','jackettt',2000,0.15,'linear')
-	end
-end
